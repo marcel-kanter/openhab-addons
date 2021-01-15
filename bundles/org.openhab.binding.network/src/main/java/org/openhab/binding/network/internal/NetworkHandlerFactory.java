9bdbcadf996dec3d1e161f398b8e82b71a3dbf12
Marcel Kanter <marcel.kanter@googlemail.com>
2021-01-15 22:17:36 +0100

    Rewind to the beginning with a device thing skeleton only.

--- bundles/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/NetworkHandlerFactory.java
+++ bundles/org.openhab.binding.network/src/main/java/org/openhab/binding/network/internal/NetworkHandlerFactory.java
@@ -12,78 +12,44 @@
  */
 package org.openhab.binding.network.internal;
 
-import java.util.Map;
+import static org.openhab.binding.network.internal.NetworkBindingConstants.*;
+
+import java.util.Set;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
-import org.openhab.binding.network.internal.handler.NetworkHandler;
-import org.openhab.binding.network.internal.handler.SpeedTestHandler;
-import org.openhab.core.config.core.Configuration;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.ThingTypeUID;
 import org.openhab.core.thing.binding.BaseThingHandlerFactory;
 import org.openhab.core.thing.binding.ThingHandler;
 import org.openhab.core.thing.binding.ThingHandlerFactory;
-import org.osgi.service.component.ComponentContext;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Modified;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * The handler factory retrieves the binding configuration and is responsible for creating
- * PING_DEVICE and SERVICE_DEVICE handlers.
+ * The {@link NetworkHandlerFactory} is responsible for creating things and thing
+ * handlers.
  *
- * @author David Graeff - Initial contribution
+ * @author Marcel Kanter - Initial contribution
  */
 @NonNullByDefault
-@Component(service = ThingHandlerFactory.class, configurationPid = "binding.network")
+@Component(configurationPid = "binding.network", service = ThingHandlerFactory.class)
 public class NetworkHandlerFactory extends BaseThingHandlerFactory {
-    final NetworkBindingConfiguration configuration = new NetworkBindingConfiguration();
 
-    private final Logger logger = LoggerFactory.getLogger(NetworkHandlerFactory.class);
+    private static final Set<ThingTypeUID> SUPPORTED_THING_TYPES_UIDS = Set.of(THING_TYPE_DEVICE);
 
     @Override
     public boolean supportsThingType(ThingTypeUID thingTypeUID) {
-        return NetworkBindingConstants.SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
-    }
-
-    // The activate component call is used to access the bindings configuration
-    @Activate
-    protected void activate(ComponentContext componentContext, Map<String, Object> config) {
-        super.activate(componentContext);
-        modified(config);
-    }
-
-    @Override
-    @Deactivate
-    protected void deactivate(ComponentContext componentContext) {
-        super.deactivate(componentContext);
-    }
-
-    @Modified
-    protected void modified(Map<String, Object> config) {
-        // We update instead of replace the configuration object, so that if the user updates the
-        // configuration, the values are automatically available in all handlers. Because they all
-        // share the same instance.
-        configuration.update(new Configuration(config).as(NetworkBindingConfiguration.class));
-        logger.debug("Updated binding configuration to {}", configuration);
+        return SUPPORTED_THING_TYPES_UIDS.contains(thingTypeUID);
     }
 
     @Override
     protected @Nullable ThingHandler createHandler(Thing thing) {
         ThingTypeUID thingTypeUID = thing.getThingTypeUID();
 
-        if (thingTypeUID.equals(NetworkBindingConstants.PING_DEVICE)
-                || thingTypeUID.equals(NetworkBindingConstants.BACKWARDS_COMPATIBLE_DEVICE)) {
-            return new NetworkHandler(thing, false, configuration);
-        } else if (thingTypeUID.equals(NetworkBindingConstants.SERVICE_DEVICE)) {
-            return new NetworkHandler(thing, true, configuration);
-        } else if (thingTypeUID.equals(NetworkBindingConstants.SPEEDTEST_DEVICE)) {
-            return new SpeedTestHandler(thing);
+        if (thingTypeUID.equals(THING_TYPE_DEVICE)) {
+            return new NetworkDeviceHandler(thing);
         }
+
         return null;
     }
 }
